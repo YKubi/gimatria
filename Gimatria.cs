622f7ea38f86fc51c7da501ed2d70c366d66229f
Yeshurun Kubi <44770058+YKubi@users.noreply.github.com>
2021-06-01 13:24:16 +0300

    Update Gimatria.cs
    Fixed the dictionaries to be static.
    Arranged the code in regions.

--- Gimatria.cs
+++ Gimatria.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -9,6 +9,8 @@ namespace FindAddIn
     /// </summary>
     public class Gimatria
     {
+        #region Functions
+
         /// <summary>
         /// פעולת גימטריה הפוך מספר לאותיות עברית
         /// </summary>
@@ -150,41 +152,94 @@ namespace FindAddIn
                 return number;
             }
 
-            // מילון גימטריה עברית למספר
-            Dictionary<string, int> hebToNum = new Dictionary<string, int>();
-
-            hebToNum.Add("א", 1);
-            hebToNum.Add("ב", 2);
-            hebToNum.Add("ג", 3);
-            hebToNum.Add("ד", 4);
-            hebToNum.Add("ה", 5);
-            hebToNum.Add("ו", 6);
-            hebToNum.Add("ז", 7);
-            hebToNum.Add("ח", 8);
-            hebToNum.Add("ט", 9);
-            hebToNum.Add("י", 10);
-            hebToNum.Add("כ", 20);
-            hebToNum.Add("ל", 30);
-            hebToNum.Add("מ", 40);
-            hebToNum.Add("נ", 50);
-            hebToNum.Add("ס", 60);
-            hebToNum.Add("ע", 70);
-            hebToNum.Add("פ", 80);
-            hebToNum.Add("צ", 90);
-            hebToNum.Add("ק", 100);
-            hebToNum.Add("ר", 200);
-            hebToNum.Add("ש", 300);
-            hebToNum.Add("ת", 400);
-
             for (int i = 0; i < text.Length; i++)
             {
-                number += hebToNum[text.Substring(i, 1)];
+                number += HebToNumDictionary[text.Substring(i, 1)];
             }
 
             return number;
         }
 
-        #region Private Functions
+        #endregion Functions
+
+        #region Convert dictionaries & strings
+
+        /// <summary>
+        /// מילון גימטריה עברית למספר
+        /// </summary>
+        private static readonly Dictionary<string, int> HebToNumDictionary = new Dictionary<string, int>
+        {
+            {"א", 1}, {"ב", 2}, {"ג", 3}, {"ד", 4}, {"ה", 5}, {"ו", 6}, {"ז", 7}, {"ח", 8}, {"ט", 9},
+            {"י", 10}, {"כ", 20}, {"ל", 30}, {"מ", 40}, {"נ", 50}, {"ס", 60}, {"ע", 70}, {"פ", 80}, {"צ", 90},
+            {"ק", 100}, {"ר", 200}, {"ש", 300}, {"ת", 400}
+        };
+
+        /// <summary>
+        /// מילון המרה של אותיות עברית ביוניקוד לאותיות נכונות
+        /// </summary>
+        private static readonly Dictionary<string, string> AlphabeticDictionary = new Dictionary<string, string>
+        {
+            {"ם", "מ"}, {"ן", "נ"}, {"ץ", "ס"}, {"ף", "ע"}, {"ך", "פ"}, {"\u05F0", "וו"}, {"\u05F1", "וי"},
+            {"\u05F2", "יי"}, {"\uFB1D", "י"}, {"\uFB1E", ""}, {"\uFB1F", "יי"},
+            {"\uFB20", "ע"}, {"\uFB21", "א"}, {"\uFB22", "ד"}, {"\uFB23", "ה"}, {"\uFB24", "כ"}, {"\uFB25", "ל"}, {"\uFB26", "מ"},
+            {"\uFB27", "ר"}, {"\uFB28", "ת"}, {"\uFB29", ""}, {"\uFB2A", "ש"}, {"\uFB2B", "ש"}, {"\uFB2C", "ש"}, {"\uFB2D", "ש"},
+            {"\uFB2E", "א"}, {"\uFB2F", "א"}, {"\uFB30", "א"}, {"\uFB31", "ב"}, {"\uFB32", "ג"}, {"\uFB33", "ד"}, {"\uFB34", "ה"},
+            {"\uFB35", "ו"}, {"\uFB36", "ז"}, {"\uFB37", ""}, {"\uFB38", "ט"}, {"\uFB39", "י"}, {"\uFB3A", "כ"}, {"\uFB3B", "כ"},
+            {"\uFB3C", "ל"}, {"\uFB3D", ""}, {"\uFB3E", "מ"}, {"\uFB3F", ""}, {"\uFB40", "נ"}, {"\uFB41", "ס"}, {"\uFB42", ""},
+            {"\uFB43", "פ"}, {"\uFB44", "פ"}, {"\uFB45", ""}, {"\uFB46", "צ"}, {"\uFB47", "ק"}, {"\uFB48", "ר"}, {"\uFB49", "ש"},
+            {"\uFB4A", "ת"}, {"\uFB4B", "ו"}, {"\uFB4C", "ב"}, {"\uFB4D", "כ"}, {"\uFB4E", "פ"}, {"\uFB4F", "אל"}
+        };
+
+        /// <summary>
+        /// יחידות באותיות
+        /// תא 0 ריק, תא 1 מחזיר א תא 2 ב וכו
+        /// </summary>
+        private static readonly string[] unitsLetters =
+        {
+            "",
+            "א",
+            "ב",
+            "ג",
+            "ד",
+            "ה",
+            "ו",
+            "ז",
+            "ח",
+            "ט"
+        };
+
+        /// <summary>
+        /// עשרות באותיות
+        /// תא 0 ריק, תא 1 מחזיר י תא 2 כ וכו
+        /// </summary>
+        private static readonly string[] tensLetters =
+        {
+            "",
+            "י",
+            "כ",
+            "ל",
+            "מ",
+            "נ",
+            "ס",
+            "ע",
+            "פ",
+            "צ",
+        };
+
+        /// <summary>
+        /// מאות באותיות
+        /// תא 0 ריק, תא 1 מחזיר ק תא 2 ר וכו
+        /// </summary>
+        private static readonly string[] hundredsLetters =
+        {
+            "",
+            "ק",
+            "ר",
+            "ש",
+            "ת"
+        };
+
+        #endregion Convert dictionaries & strings
 
         /// <summary>
         /// מנקה את הטקסט ומסדר שיהיה אותיות א-ב רגילות
@@ -205,76 +260,13 @@ namespace FindAddIn
             Regex regex = new Regex("[^\u05D0-\u05F2\uFB1D-\uFB4F]");
             result = regex.Replace(text, "");
 
-            // מילון המרה לאותיות מיוחדות
-            Dictionary<string, string> alphabtic = new Dictionary<string, string>();
-
-            alphabtic.Add("ם", "מ");
-            alphabtic.Add("ן", "נ");
-            alphabtic.Add("ץ", "ס");
-            alphabtic.Add("ף", "ע");
-            alphabtic.Add("ך", "פ");
-            alphabtic.Add("\u05F0", "וו");
-            alphabtic.Add("\u05F1", "וי");
-            alphabtic.Add("\u05F2", "יי");
-            alphabtic.Add("\uFB1D", "י");
-            alphabtic.Add("\uFB1E", "");
-            alphabtic.Add("\uFB1F", "יי");
-            alphabtic.Add("\uFB20", "ע");
-            alphabtic.Add("\uFB21", "א");
-            alphabtic.Add("\uFB22", "ד");
-            alphabtic.Add("\uFB23", "ה");
-            alphabtic.Add("\uFB24", "כ");
-            alphabtic.Add("\uFB25", "ל");
-            alphabtic.Add("\uFB26", "מ");
-            alphabtic.Add("\uFB27", "ר");
-            alphabtic.Add("\uFB28", "ת");
-            alphabtic.Add("\uFB29", "");
-            alphabtic.Add("\uFB2A", "ש");
-            alphabtic.Add("\uFB2B", "ש");
-            alphabtic.Add("\uFB2C", "ש");
-            alphabtic.Add("\uFB2D", "ש");
-            alphabtic.Add("\uFB2E", "א");
-            alphabtic.Add("\uFB2F", "א");
-            alphabtic.Add("\uFB30", "א");
-            alphabtic.Add("\uFB31", "ב");
-            alphabtic.Add("\uFB32", "ג");
-            alphabtic.Add("\uFB33", "ד");
-            alphabtic.Add("\uFB34", "ה");
-            alphabtic.Add("\uFB35", "ו");
-            alphabtic.Add("\uFB36", "ז");
-            alphabtic.Add("\uFB37", "");
-            alphabtic.Add("\uFB38", "ט");
-            alphabtic.Add("\uFB39", "י");
-            alphabtic.Add("\uFB3A", "כ");
-            alphabtic.Add("\uFB3B", "כ");
-            alphabtic.Add("\uFB3C", "ל");
-            alphabtic.Add("\uFB3D", "");
-            alphabtic.Add("\uFB3E", "מ");
-            alphabtic.Add("\uFB3F", "");
-            alphabtic.Add("\uFB40", "נ");
-            alphabtic.Add("\uFB41", "ס");
-            alphabtic.Add("\uFB42", "");
-            alphabtic.Add("\uFB43", "פ");
-            alphabtic.Add("\uFB44", "פ");
-            alphabtic.Add("\uFB45", "");
-            alphabtic.Add("\uFB46", "צ");
-            alphabtic.Add("\uFB47", "ק");
-            alphabtic.Add("\uFB48", "ר");
-            alphabtic.Add("\uFB49", "ש");
-            alphabtic.Add("\uFB4A", "ת");
-            alphabtic.Add("\uFB4B", "ו");
-            alphabtic.Add("\uFB4C", "ב");
-            alphabtic.Add("\uFB4D", "כ");
-            alphabtic.Add("\uFB4E", "פ");
-            alphabtic.Add("\uFB4F", "אל");
-
             string resultNew = "";
 
             for (int i = 0; i < result.Length; i++)
             {
                 string value = "";
                 string s = result.Substring(i, 1);
-                if (alphabtic.TryGetValue(s, out value))
+                if (AlphabeticDictionary.TryGetValue(s, out value))
                 {
                     resultNew += value;
                 }
@@ -286,56 +278,5 @@ namespace FindAddIn
 
             return resultNew;
         }
-
-        /// <summary>
-        /// יחידות באותיות
-        /// תא 0 ריק, תא 1 מחזיר א תא 2 ב וכו
-        /// </summary>
-        private static string[] unitsLetters =
-        {
-            "",
-            "א",
-            "ב",
-            "ג",
-            "ד",
-            "ה",
-            "ו",
-            "ז",
-            "ח",
-            "ט"
-        };
-
-        /// <summary>
-        /// עשרות באותיות
-        /// תא 0 ריק, תא 1 מחזיר י תא 2 כ וכו
-        /// </summary>
-        private static string[] tensLetters =
-        {
-            "",
-            "י",
-            "כ",
-            "ל",
-            "מ",
-            "נ",
-            "ס",
-            "ע",
-            "פ",
-            "צ",
-        };
-
-        /// <summary>
-        /// מאות באותיות
-        /// תא 0 ריק, תא 1 מחזיר ק תא 2 ר וכו
-        /// </summary>
-        private static string[] hundredsLetters =
-        {
-            "",
-            "ק",
-            "ר",
-            "ש",
-            "ת"
-        };
-
-        #endregion Private Functions
     }
 }
